a49838db03b73d5ac1f99e6ae676c456b6fe13ed
TMN <t.martin@adsoftware.fr>
2024-10-26 11:09:02 +0200

    fix: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    cors({
+      origin: "*",
+    })
+  )
+  .get("/", () => ({
+    greeting: "Hello, from the server!",
+  }))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
